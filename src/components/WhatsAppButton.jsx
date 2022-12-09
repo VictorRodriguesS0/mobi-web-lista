4189c526282de29f08232b547e25f32125e82478
Victor Rodrigues <victorrsilva17@gmail.com>
2022-12-09 01:34:21 -0300

    Atualizando Zap

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { isBrowser } from 'react-device-detect';
-import WhatsAppLogo from '../assets/icons/whatsapp-logo';
+import React from "react";
+import { isBrowser } from "react-device-detect";
+import WhatsAppLogo from "../assets/icons/whatsapp-logo";
 
 const WhatsAppButton = () => (
   <div
     className="floating-button row"
-    style={{ right: isBrowser ? 'calc(50vw - 500px)' : 40, bottom: 120 }}
+    style={{ right: isBrowser ? "calc(50vw - 500px)" : 40, bottom: 120 }}
   >
     <button
       type="button"
       className="custom-button zoom-hover floating-shadow"
       style={{
-        backgroundColor: '#4AC959',
-        borderRadius: '100%',
+        backgroundColor: "#4AC959",
+        borderRadius: "100%",
         height: 50,
         width: 50,
       }}
-      onClick={() => { window.open('https://api.whatsapp.com/send?phone=+5561986078550', '_blank'); }}
+      onClick={() => {
+        window.open("https://wa.me/5561995100506", "_blank");
+      }}
     >
-      {WhatsAppLogo({ color: 'white', height: 30, width: 30 })}
+      {WhatsAppLogo({ color: "white", height: 30, width: 30 })}
     </button>
   </div>
 );
